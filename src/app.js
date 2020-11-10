0d83dba8f98eb4342d6737fd413718032422c949
Andrew Coleburn <arcoleburn@gmail.com>
2020-11-10 15:32:56 -0500

    set up and added first get endpt

---
+++ src/app.js
@@ -0,0 +1,48 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const entriesRouter = require('./journalEntries/entries-router');
+const EntriesService = require('./journalEntries/entries-service');
+const app = express();
+
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.get('/api/entries/:userId', (req, res, next) => {
+  console.log(req.params);
+  EntriesService.getEntriesForUser(
+    req.app.get('db'),
+    req.params.userId
+  ).then((entries) =>
+    res.json(
+      entries.map((entry) => {
+        console.log('entry from get', EntriesService.serializeEntry(entry));
+        return EntriesService.serializeEntry(entry);
+      })
+    )
+  );
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.log(error);
+    response = { message: error.messager, error };
+  }
+  res.status(500).json(response);
+});
+module.exports = app;
